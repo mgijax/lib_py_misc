143f224e7ef8364b25499da0044df678171167c4
Lori Corbani <lori.corbani@jax.org>
2020-04-06 11:41:38 -0400

    TR13204/Infrastructure/python 3

--- rcdlib.py
+++ rcdlib.py
@@ -583,7 +583,7 @@ def readAndParseLine (
 
                         eqPos = line.find ('=')
                         if eqPos != -1:
-                                name = line[:egPos].rstrip()
+                                name = line[:eqPos].restrip()
                                 value = line[eqPos+1:].lstrip()
 
                                 # if the value isn't marked as a continued
