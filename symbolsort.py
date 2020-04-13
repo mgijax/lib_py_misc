e7d39ad248c0e411e79a3569661db8ec7633c377
Lori Corbani <lori.corbani@jax.org>
2020-04-13 08:12:52 -0400

    TR13204/Infrastructure/python 3

--- symbolsort.py
+++ symbolsort.py
@@ -1,60 +1,66 @@
-# Name: symbolsort.py
-# Purpose: provide a splitter() function that can be shared across Python products to
-#    consistently split an alphanumeric string into a tuple of integers and strings for
-#    sorting.
+# Purpose: provide a nomenCompare() function to replace the byNumeric(),
+#	bySymbol(), and byFilename() functions in the WI's version of
+#	mgd_utils.py
+# Notes:
+#	At some point, this mgd_utils should be pulled out of the WI and CCR.
+#	The code here should supercede that in the WI, as noted in the Purpose
+#	above.  This remedies a bug in the WI whereby Ren1 == ren2 for sorting
+#	purposes.
+#	This file is now also used in the ratpages.  It should be pulled out
+#	of there as well.
 
 # global dictionaries used by splitter() for speedy lookups:
 
-sdict = {       '' : ('')       }
-digits = {      '1' : 1,        '2' : 1,        '3' : 1,        '4' : 1,
-                '5' : 1,        '6' : 1,        '7' : 1,        '8' : 1,
-                '9' : 1,        '0' : 1 }
+sdict = {	'' : ('')	}
+digits = {	'1' : 1,	'2' : 1,	'3' : 1,	'4' : 1,
+                '5' : 1,	'6' : 1,	'7' : 1,	'8' : 1,
+                '9' : 1,	'0' : 1	}
 
 def splitter (s):
-        # Purpose: split string 's' into a tuple of strings and integers,
-        #   representing the contents of 's' for sorting purposes
-        # Returns: tuple containing a list of strings and integers
-        # Assumes: s is a string or None
+        # Purpose: split str.'s' into a tuple of str. and integers,
+        #	representing the contents of 's'
+        # Returns: tuple containing a list of str. and integers
+        # Assumes: s is a string
         # Effects: nothing
         # Throws: nothing
-        # Notes: Because Python 3.7 does not allow strings to be compared with integers,
-        #    we must ensure that all tuples have the same ordering of integers and strings.
-        #    (So even for ones that would begin with a string, we'll prepend an integer to
-        #    force it to appear after those beginning with integers.)
         # Examples:
-        #       'Ren1' ==> (9999999, 'ren', 1)
-        #       'abc123def' ==> (9999999, 'abc', 123, 'def')
-        #       '789xyz32' ==> (789, 'xyz', 32)
+        #	'Ren1' ==> ('Ren', 1)
+        #	'abc123def' ==> ('abc', 123, 'def')
 
         global sdict
-
-        if s == None:
-            return (99999999,)
         if s in sdict:
                 return sdict[s]
         last = 0
         items = []
-        sl = s.lower ()
+        sl = str.lower (s)
         in_digits = sl[0] in digits
         for i in range(0, len(sl)):
                 if (sl[i] in digits) != in_digits:
                         if in_digits:
-                                items.append (int(sl[last:i]))
+                                items.append (str.atoi(sl[last:i]))
                         else:
                                 items.append (sl[last:i])
                         last = i
                         in_digits = not in_digits
         if in_digits:
-                items.append (int(sl[last:]))
+                items.append (str.atoi (sl[last:]))
         else:
                 items.append (sl[last:])
-                
-        if type(items[0]) != int:
-            items.insert(0, 9999999)
-
         sdict[s] = tuple(items)
         return sdict[s]
 
+def nomenCompare (s1, s2):
+        # Purpose: compare str. s1 and s2
+        # Returns: an integer;	negative if s1 < s2,
+        #			0 if s1 == s2,
+        #			positive if s1 > s2
+        # Assumes: s1 and s2 are str.
+        # Effects: nothing
+        # Throws: nothing
+        # Notes: considers numerics which may be included in s1 & s2, so that
+        #	'Abc9' sorts before 'Abc10', for example
+
+        return cmp(splitter(s1), splitter(s2))
 #
 # Warranty Disclaimer and Copyright Notice
 # 
@@ -74,4 +80,3 @@ def splitter (s):
 # Copyright (c) 1996, 1999, 2002 by The Jackson Laboratory
 # All Rights Reserved
 #
-
